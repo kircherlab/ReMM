92831c0198b27e7947cb195fb5de365be0d3feca
nazaretl <nazaretl@hu-berlin.de>
2021-03-19 12:43:25 +0100

    Removed all files that are in the .gitignore

--- Snakemake/scripts/numTFBSConserved.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-input_file = str(snakemake.input)
-df = pd.read_csv(input_file,header = None,sep ='\t')
-CHROMS = ['chr'+str(s)  for s in list(range(1,23))+['Y','X']]
-for c in CHROMS:
-    file = 'input/features/hg38/numTFBSConserved/numTFBSConserved.'+c+'.bed.gz.vartmp'
-    d = df[df[0]==c]
-    d.to_csv(file, header = None,index = None, sep='\t')
